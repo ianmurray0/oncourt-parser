9eac780a2c84a57af9ac6dc017f0fc7ad578aae1
ianmurray0 <ianmurray0@gmail.com>
2022-04-11 15:02:51 -0700

    Update PlayByPlayDriver.py

--- PlayByPlayDriver.py
+++ PlayByPlayDriver.py
@@ -1,11 +1,9 @@
-from sqlite3 import Cursor
 import sys, os, time, re
 import pandas as pd
 import pyodbc
-
 from OnCourtDriver import get_file_paths, clean_player_name
 from parsers.PlayParser import parse_entry
-from progress_bar.ProgressBar import update_progress 
+from progress_bar.ProgressBar import update_progress
 
 def main():
     """
@@ -17,19 +15,15 @@ def main():
 
     python PlayByPlayDriver.py [input_file/input_path] [output_path]
     """
-
     try: 
-        if (len(sys.argv) != 3):
+        if (len(sys.argv) != 2):
             raise SyntaxError("Invalid input arguments. Input command should be \
-                python PlayByPlayDriver.py [input_file/input_path] [output_path]\
+                python PlayByPlayDriver.py [input_file/input_path] \
                 ")
         if (not os.path.exists(sys.argv[1])):
-            raise FileNotFoundError(sys.argv[1] + " does not exist")
-        if (os.path.exists(sys.argv[2])):
-            raise FileExistsError(sys.argv[2] + " already exists")
-        global conn
-        conn = connectToSQLDatabase()
-        run(sys.argv[1], sys.argv[2])
+            raise FileNotFoundError(sys.argv[1] + " does not exist")   
+        run(sys.argv[1])
+
     except SyntaxError as e:
         print(e)
     except FileNotFoundError as e:
@@ -37,7 +31,7 @@ def main():
     except FileExistsError as e:
         print(e)
 
-def run(input_path: str, output_path: str):
+def run(input_path: str):
     """
     This function is responsible for finding all the files specified by 'input_path',
     parsing the contents of the file, calling the PlayParser, then processing that
@@ -55,18 +49,15 @@ def run(input_path: str, output_path: str):
                  a folder containing .xlsl files.
     output_path: The output path where we plan on storing the collected data.
     """
+    global conn 
+    conn = connectToSQLDatabase()
     files = get_file_paths(input_path)
     # Output - an array of Pandas.DataFrames
-    output = []
     i = 1
     for file_path in files:
         print("Parsing " + file_path + " (" + str(i) + "/" + str(len(files)) + ")")
         file_contents = open_file(file_path)
-        output.append(get_play_data(file_contents))
-    
-    os.mkdir(output_path)
-    for data in output:
-        data.to_excel(output_path +"/out.xlsx", index=False)
+        get_play_data(file_contents)
 
 def open_file(path: str) -> pd.DataFrame:
     """
@@ -102,24 +93,20 @@ def get_play_data(df: pd.DataFrame):
     --------
     A Pandas.DataFrame containing usable data obtained from the play parser.
     """
-    frames = []
     for i, row in df.iterrows():
         # Run PlayParser
         # Call function to parse output of PlayParser
         # Add function output to output DataFrame
         p1_name = clean_player_name(row[0])
         p2_name = clean_player_name(row[1])
-        populateTableIN_PLAY_DATA(p1_name)
-        populateTableIN_PLAY_DATA(p2_name)
+        populateTablePLAYER(p1_name)
+        populateTablePLAYER(p2_name)
         date = str(row[3])[0:10]
         entry_key = p1_name + " " + p2_name + " " + date
-
         if (isinstance(row[15], str) and not re.match("\s+", row[15])):
             df_play = parse_entry(row[15])
-            frames.append(parse_play_dataframe(df_play, entry_key))
+            parse_play_dataframe(df_play, entry_key)
         update_progress(i / len(df.index))
-
-    return pd.concat(frames)
 
 def parse_play_dataframe(df: pd.DataFrame, key: str) -> pd.DataFrame:
     """
@@ -149,51 +136,21 @@ def parse_play_dataframe(df: pd.DataFrame, key: str) -> pd.DataFrame:
     """ 
     output_columns = ["Key", "SetNo", "P1GamesWon", "P2GamesWon", "SetWinner", 
         "GameNo", "GameWinner", "PointNumber", "PointWinner", "PointServer", "Score"]
-    output_df = pd.DataFrame(columns=output_columns)
-    output_df = output_df.iloc[1:]
 
     set_no = 1
     game_no = 0
     point_no = 0
     point_server = 0
-
     for i, row in df.iterrows():
         set_games = row[1].split("-")
         if (len(set_games) == 2):
             set_winner = 1 if set_games[0] > set_games[1] else 2
             if (row[0] == "EndGame"):
-                output_df = output_df.append({
-                    "Key": key,
-                    "SetNo": set_no,
-                    "P1GamesWon": set_games[0],
-                    "P2GamesWon": set_games[1][0],
-                    "SetWinner": 0,
-                    "GameNo": game_no,
-                    "GameWinner": set_winner,
-                    "PointNumber": point_no, 
-                    "PointWinner": set_winner,
-                    "PointServer": point_server,
-                    "Score": row[0],
-                }, ignore_index=True)
-                populateTableIN_PLAY_DATA(key,set_no,set_games[0],set_games[1][0],\
-                    0,game_no,set_winner,point_no,set_winner,point_server,row[0])
+                populateTableIN_PLAY_DATA(key,set_no,set_games[0],set_games[1],0,game_no,set_winner,point_no,set_winner,point_server,row[0])
+                
                 game_no += 1
             elif (row[0] == "EndSet" ):
-                output_df = output_df.append({
-                    "Key": key,
-                    "SetNo": set_no,
-                    "P1GamesWon": set_games[0],
-                    "P2GamesWon": set_games[1][0],
-                    "SetWinner": set_winner,
-                    "GameNo": game_no,
-                    "GameWinner": set_winner,
-                    "PointNumber": point_no, 
-                    "PointWinner": set_winner,
-                    "PointServer": point_server,
-                    "Score": row[0]
-                }, ignore_index=True)
-                #populateTableIN_PLAY_DATA(key,set_no,set_games[0],set_games[1][0],\
-                #set_winner,game_no,set_winner,point_no,set_winner,point_server,row[1])
+                populateTableIN_PLAY_DATA(key,set_no,set_games[0],set_games[1],set_winner,game_no,set_winner,point_no,set_winner,point_server,row[1])
                 game_no = 0
                 set_no += 1
             elif (row[0] == "End"):
@@ -208,24 +165,8 @@ def parse_play_dataframe(df: pd.DataFrame, key: str) -> pd.DataFrame:
                     if (remove_brackets.index('*') == 0): point_server = 1
                 except ValueError:
                     point_server = -1
-
-                output_df = output_df.append({
-                    "Key": key,
-                    "SetNo": set_no,
-                    "P1GamesWon": set_games[0],
-                    "P2GamesWon": set_games[1][0],
-                    "SetWinner": 0,
-                    "GameNo": game_no,
-                    "GameWinner": 0,
-                    "PointNumber": point_no, 
-                    "PointWinner": 1 if games[0] > games[1] else 2,
-                    "PointServer": point_server,
-                    "Score": row[0],
-                }, ignore_index=True) 
-                #populateTableIN_PLAY_DATA(key,set_no,set_games[0],set_games[1][0], \
-                # 0,game_no,0,point_no, 1 if games[0] > games[1] else 2, point_server, row[0])
+                populateTableIN_PLAY_DATA(key,set_no,set_games[0],set_games[1],0,game_no,0,point_no, 1 if games[0] > games[1] else 2, point_server, row[0])
             point_no += 1
-    return output_df
 
 def connectToSQLDatabase():
     """
@@ -239,27 +180,28 @@ def connectToSQLDatabase():
     driver= '{ODBC Driver 17 for SQL Server}'
     return pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 
-def populateTableIN_PLAY_DATA(Key,SetNo,P1GamesWon,P2GamesWon,SetWinner,GameNo,GameWinner,\
-    PointNumber,PointWinner,PointServer,Score):
+def populateTablePLAYER(PlayerName):
+    sql = "EXEC [TennisModelling].[dbo].[SP_PLAYER_NAME_ADD] ?"
     cursor = conn.cursor()
-    sqlInsert = "INSERT INTO [IN_PLAY_DATA]([IN_PLAY_DATA_KEY], [IN_PLAY_DATA_SET_NUMBER], \
-        [IN_PLAY_DATA_PLAYER_ONE_GAMES_WON], [IN_PLAY_DATA_PLAYER_TWO_GAMES_WON],[IN_PLAY_DATA_SET_WINNER],"\
-                  +"[IN_PLAY_DATA_GAME_NUMBER], [IN_PLAY_DATA_GAME_WINNER], [IN_PLAY_DATA_POINT_NUMBER],\
-                       [IN_PLAY_DATA_POINT_WINNER], [IN_PLAY_DATA_POINT_SERVER],[IN_PLAY_DATA_SCORE]) "\
-                  +"VALUES(?,?,?,?,?,?,?,?,?,?,?)"
-    parameters = (Key,SetNo,P1GamesWon,P2GamesWon,SetWinner,GameNo,GameWinner,PointNumber,\
-        PointWinner,PointServer,Score)
-    cursor.execute(sqlInsert, parameters)
+    #sqlInsert = "INSERT INTO [PLAYER]([PLAYER_NAME]) " \
+    #            "VALUES(?)" \
+    #            "WHERE NOT EXISTS (SELECT [PLAYER_ID] FROM [PLAYER] WHERE [PLAYER_NAME] = (?)"
+    parameters = (PlayerName)
+    cursor.execute(sql, parameters)
     conn.commit()
 
-def populateTablePLAYER(PlayerName):
+def populateTableIN_PLAY_DATA(Key,SetNo,P1GamesWon,P2GamesWon,SetWinner,GameNo,GameWinner,PointNumber,PointWinner,PointServer,Score):
+    """
+    This function populates the IN_PLAY_DATA table of the Azure SQL Database
+    """
     cursor = conn.cursor()
-    sqlInsert = "INSERT INTO [PLAYER](PLAYER_NAME)" \
-                  +"VALUES(?)"
-    parameters = (PlayerName)
+    sqlInsert = "INSERT INTO [IN_PLAY_DATA]([IN_PLAY_DATA_KEY], [IN_PLAY_DATA_SET_NUMBER], [IN_PLAY_DATA_PLAYER_ONE_GAMES_WON], [IN_PLAY_DATA_PLAYER_TWO_GAMES_WON],[IN_PLAY_DATA_SET_WINNER],"\
+                  +"[IN_PLAY_DATA_GAME_NUMBER], [IN_PLAY_DATA_GAME_WINNER], [IN_PLAY_DATA_POINT_NUMBER], [IN_PLAY_DATA_POINT_WINNER], [IN_PLAY_DATA_POINT_SERVER],[IN_PLAY_DATA_SCORE]) "\
+                  +"VALUES(?,?,?,?,?,?,?,?,?,?,?)"
+    parameters = (Key,SetNo,P1GamesWon,P2GamesWon,SetWinner,GameNo,GameWinner,PointNumber,PointWinner,PointServer,Score)
     cursor.execute(sqlInsert, parameters)
-    conn.commit()
     
+    conn.commit()
 
 if __name__ == "__main__":
     start = time.time()
